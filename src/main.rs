af2c248933b63c60fe50773c39742119201dafbe
tdl1304 <tommy.1304@hotmail.com>
2021-03-17 19:00:35 +0100

    Setup local TCP connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::thread::sleep;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let t1 = thread::spawn(move || {
+        while (true) {
+            sleep(Duration::from_millis(5000));
+            println!("Trying to connect");
+            let stream = TcpStream::connect("127.0.0.1:8080");
+        }
+    });
+
+    println!("b4");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("aft");
+    // accept connections and process them serially
+    while(true) {
+        let client = listener.accept().unwrap();
+        println!("Client connected {}", client.1.to_string())
+    }
+
+
 }
+
+
+
