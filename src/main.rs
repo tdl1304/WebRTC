4f2f691390725f82490d6f6818349a5999f0ce40
tdl1304 <tommy.1304@hotmail.com>
2021-03-18 20:09:56 +0100

    updated CI

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ use std::thread::sleep;
 use std::time::Duration;
 
 fn main() {
-    let something = stun_handler::Headers{
+    let something = stun_handler::Headers {
         message_type: 0,
         message_length: 0,
         magic_cookie: 0,
-        transaction_id: 0
+        transaction_id: 0,
     };
 
     let t1 = thread::spawn(move || {
-        while (true) {
+        loop {
             sleep(Duration::from_millis(5000));
             println!("Trying to connect");
             let stream = TcpStream::connect("127.0.0.1:8080");
@@ -25,7 +25,7 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     println!("aft");
     // accept connections and process them serially
-    while(true) {
+    loop {
         let client = listener.accept().unwrap();
         println!("Client connected {}", client.1.to_string())
     }
