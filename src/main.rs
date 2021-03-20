819f0ffa07bad2a99cd285af981deed3cae6344c
tdl1304 <tommy.1304@hotmail.com>
2021-03-20 03:12:44 +0100

    changed ip

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ mod tcp_handler;
 mod udp_handler;
 
 fn main() {
-    let t1 = udp_handler::init(String::from("127.0.0.1"));
-    let t2 = tcp_handler::init(String::from("127.0.0.1"));
+    let t1 = udp_handler::init(String::from("0.0.0.0"));
+    let t2 = tcp_handler::init(String::from("0.0.0.0"));
     t1.join().unwrap();
     t2.join().unwrap();
 }
